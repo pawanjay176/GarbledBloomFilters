f52c37a3aafd3466830746bcdf80f3b5dfaf36d5
pawanjay176 <pawandhananjay@gmail.com>
2015-12-12 02:40:01 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import socket               # Import socket module
-
-s = socket.socket()         # Create a socket object
-host = '127.0.0.1' # Get local machine name
-port = 3000                # Reserve a port for your service.
-
-s.connect((host, port))
-print s.recv(1024)
-s.send("Ye lo pk0")
